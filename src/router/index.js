a74440ecf5525a4b5f8e2cbbe9bd0e923073210f
laddon <@l.zharov.00>
2023-06-12 16:55:28 +0300

    Auth

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import RegistrationView from "@/views/RegistrationView.vue";
 import i18n from '../i18n';
 import { createI18n } from 'vue-i18n';
 import PersonView from "@/views/PersonView.vue";
+import store from '../store';
 
 const routes = [
   {
@@ -47,7 +48,7 @@ const routes = [
     name: 'statistic',
     component: StatisticView,
     meta: {
-      titleKey: 'base.navbar.statistic'
+      titleKey: 'base.navbar.statistic',
     }
   },
   {
@@ -55,7 +56,9 @@ const routes = [
     name: 'login',
     component: LogInView,
     meta: {
-      titleKey: 'base.navbar.login'
+      titleKey: 'base.navbar.login',
+      requiresAuth: true,
+      requiresAuthDisallow: true
     }
   },
   {
@@ -73,12 +76,48 @@ const router = createRouter({
   routes
 });
 
-router.afterEach((to) => {
-  const pageTitleKey = to.meta.titleKey;
-  if (pageTitleKey) {
-    const pageTitle = getTitleTranslation(pageTitleKey);
-    document.title = pageTitle;
-  }
-});
+
+const setupRouter = () => {
+
+  router.beforeEach((to, from, next) => {
+
+     const isLoggedIn = store.getters.isLoggedIn;
+      console.log(isLoggedIn);
+    // Проверка, требуется ли аутентификация для данного маршрута
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+      // Проверяем, авторизован ли пользователь
+      if (!isLoggedIn) {
+        console.log('User not auth');
+        if (to.matched.some(record => record.meta.requiresAuthDisallow)) {
+          next();
+        }
+        else {
+          next('/login');
+        }
+      } else {
+        if (to.matched.some(record => record.meta.requiresAuthDisallow)) {
+          // Если у маршрута указана meta.requiresAuthDisallow, запрещаем доступ авторизованным пользователям
+          next('/'); // Можете перенаправить на другую страницу, если нужно
+        } else {
+          // Разрешаем доступ авторизованным пользователям
+          next();
+        }
+      }
+    } else {
+      // Маршрут не требует аутентификации, продолжаем нормально
+      next();
+    }
+  });
+
+  router.afterEach((to) => {
+    const pageTitleKey = to.meta.titleKey;
+    if (pageTitleKey) {
+      const pageTitle = getTitleTranslation(pageTitleKey);
+      document.title = pageTitle;
+    }
+  });
+};
+
+setupRouter();
 
 export default router;
